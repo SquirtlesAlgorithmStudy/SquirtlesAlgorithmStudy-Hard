e01304dbdcd1558a151006cd231e915d13a6dd39
Minseo Kang <81473751+Mingosnake@users.noreply.github.com>
2025-02-11 21:57:45 +0900

    [민서] leetcode JumpGameII

--- 민서/JumpGameII.py
+++ 민서/JumpGameII.py
@@ -6,3 +6,18 @@ class Solution:
             for j in range(i, min(i + n + 1, len(nums))):
                 memo[j] = min(memo[j], memo[i] + 1)
         return memo[-1]
+
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return 0
+        memo = []
+        start = 0
+        end = min(nums[0], len(nums) - 1)
+        memo.append(end)
+        while end < len(nums) - 1:
+            mx = max(i + nums[i] for i in range(start, end + 1))
+            start, end = end + 1, mx
+            memo.append(end)
+        return len(memo)
