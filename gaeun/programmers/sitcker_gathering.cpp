de7a49a4274eaaa852c879c11d54e359a3f95e8e
ghan <hge4275@naver.com>
2025-04-22 21:56:23 +0900

    update

--- gaeun/programmers/sitcker_gathering.cpp
+++ gaeun/programmers/sitcker_gathering.cpp
@@ -10,52 +10,28 @@ int solution(vector<int> sticker)
     int N = sticker.size();
     vector<int> dp(N, 0);
     
-    // start from [0]
+    // w/o the last sticker
     dp[0] = sticker[0];
-    for(int i=0; i<=N-4; i++){
-        if(i==1) continue;
-        
+    dp[1] = sticker[1];
+    for(int i=0; i<=N-4; i++){        
         dp[i+2] = max(dp[i+2], dp[i] + sticker[i+2]);
         
         if(i+3 == N-1) continue;
         dp[i+3] = max(dp[i+3], dp[i] + sticker[i+3]);
     }
     answer = *max_element(dp.begin(), dp.end());
-    answer = max(answer, *max_element(dp.begin(), dp.end()));
-    cout << "first dp: ";
-    for(int n:dp) cout << n << " ";
-    cout << "\n";
-
     
-    // start from [1]
+    // w/ the last sticker
     dp.assign(N,0);
     dp[1] = sticker[1];
+    dp[2] = sticker[2];
     for(int i=1; i<=N-3; i++){
-        if(i == 2) continue;
-        cout << "i: " << i << "dp[i+2]: " << dp[i+2] << "dp[i] + sticker[i+2]: " << dp[i] + sticker[i+2] << "\n";
         dp[i+2] = max(dp[i+2], dp[i] + sticker[i+2]);
         
         if(i+3 >= N) continue;
         dp[i+3] = max(dp[i+3], dp[i] + sticker[i+3]);
     }
     answer = max(answer, *max_element(dp.begin(), dp.end()));
-    cout << "second dp: ";
-    for(int n:dp) cout << n << " ";
-    cout << "\n";
 
     return answer;
-}
-
-int main(){
-    int n;
-    cin >> n;
-
-    vector<int> sticker;
-    for(int i=0; i<n; i++){
-        int s;
-        cin >> s;
-        sticker.push_back(s);
-    }
-
-    cout << solution(sticker) << "\n";
 }
