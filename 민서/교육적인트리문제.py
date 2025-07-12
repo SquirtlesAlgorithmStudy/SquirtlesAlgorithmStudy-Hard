8f04f2bce7fa89958ce8dc650d406288ab9a84b2
Minseo Kang <81473751+Mingosnake@users.noreply.github.com>
2025-07-12 18:29:46 +0900

    [민서] 백준 교육적인 트리문제

--- 민서/교육적인트리문제.py
+++ 민서/교육적인트리문제.py
@@ -6,3 +6,20 @@ ans = 0
 for n in arr:
     ans += n
     print(ans)
+
+#####################################################################
+import heapq as hq
+input = open(0).readline
+N = int(input())
+graph = [[] for _ in range(N + 1)]
+for i, n in enumerate(map(int, input().split()), start=2):
+    graph[n].append(i)
+value = [0] + [*map(int, input().split())]
+h = [(-value[1], 1)]
+ans = 0
+while h:
+    v, n = hq.heappop(h)
+    ans -= v
+    for c in graph[n]:
+        hq.heappush(h, (-value[c], c))
+    print(ans)
